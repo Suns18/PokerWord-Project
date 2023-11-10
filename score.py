4822a5eef446ecfa79416bd988925fe17784466d
Pusrik <138851131+Pusrik@users.noreply.github.com>
2023-11-10 18:07:04 +0700

    Add files via upload

---
+++ score.py
@@ -0,0 +1,55 @@
+"""all mothod"""
+lstmethod = ['capitalize', 'casefold', 'center', 'count', 'encode', \
+             'endswith', 'expandtabs', 'find', 'format', 'format_map', \
+             'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', \
+             'isdigit', 'isidentifier', 'islower', 'isnumeric', \
+             'isprintable', 'isspace', 'istitle', 'isupper', 'join', \
+             'ljust', 'lower', 'lstrip', 'maketrans', 'partition', \
+             'repalce', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', \
+             'rstrip', 'split', 'splitlines', 'strip', 'swapcase', 'title', \
+             'upper', 'zfill', 'append', 'clear', 'copy', 'extend', 'insert', \
+             'pop', 'remove', 'reverse', 'sort', 'fromkeys', 'get', 'items', \
+             'keys', 'popitem', 'setdefault', 'update', 'values', 'add', \
+             'difference', 'difference_update', 'discard', 'intersection', \
+             'intersection_update', 'isdisjoint', 'issubset', \
+             'symmetric_difference', 'symmetric_difference_update', 'union', \
+             'close', 'detach', 'fileno', 'flush', 'isatty', 'read', \
+             'readable', 'readline', 'readlines', 'seek', 'seekable', 'tell', \
+             'truncate', 'writable', 'write', 'writelines', 'seed', 'getstate', \
+             'setstate', 'getrandbits', 'randrange', 'randint', 'choice', \
+             'shuffle', 'sample', 'random', 'uniform', 'triangular', 'betariate', \
+             'expovariate', 'gammavariate', 'gauss', 'lognormvariate', \
+             'normalvariate', 'vonmisesvariate', 'paretovariate', 'weibullvariate', \
+             'delete', 'head', 'patch', 'post', 'put', 'request']
+lstmethodimport = ['harmonic_mean', 'mean', 'median', 'median_grouped', 'median_high', \
+                   'median_low', 'mode', 'pstdev', 'stdev', 'pvariance', 'variance', \
+                   'acos', 'acosh', 'asin', 'asinh', 'stan', 'atan2', 'atanh', 'ceil', \
+                   'comb', 'copysign', 'cos', 'cosh', 'degrees', 'dist', 'erf', 'erfc', \
+                   'exp', 'expm1', 'fabs', 'factorial', 'floor', 'fmod', 'frexp', 'fsum', \
+                   'gamma', 'gcd', 'hypot', 'isclose', 'isfinite', 'isinf', 'isnan', \
+                   'isqrt', 'ldexp', 'lgamma', 'log', 'log10', 'log1p', 'log2', 'perm', \
+                   'pow', 'prod', 'radians', 'remainder', 'sin', 'sinh', 'sqrt', 'tan', \
+                   'tanh', 'trunc', 'phase', 'polar', 'rect', 'loads', 'dumps', \
+                   'datetime', 'search', 'sub', 'findall']
+score = { "a": 1, "b": 4, "c": 3, "d": 2, "e": 1, "f": 3, "g": 4, "h": 4, "i": 1, \
+         "j": 5, "k": 5, "l": 2, "m": 3, "n": 2, "o": 2, "p": 2, "q": 5, "r": 1, \
+            "s": 1, "t": 1, "u": 3, "v": 4, "w": 5, "x": 5, "y": 5, "z": 5}
+lstans = []
+count = 0
+while True:
+   attact = 0
+   word = input()
+   if word in lstmethod or word in lstmethodimport:
+      for char in word:
+         attact += score[char]
+      if count == 0:
+         lstans.append(word)
+         count += 1
+      if count == 3:
+         lstans.clear()
+         count = 0
+         #cardพิเศษ + 1
+      if count >= 1 and word in lstans:
+         count = 0
+   else:
+      count = 0
